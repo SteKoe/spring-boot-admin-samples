8ee1236b1ad75639a2da26f5a2921a056d9c6fa1
Stephan Köninger <stephan.koeninger@codecentric.de>
2022-09-01 11:46:16 +0200

    chore: update versions

--- spring-boot-admin-3.0/src/main/java/com/codecentric/boot/sample/Application.java
+++ spring-boot-admin-3.0/src/main/java/com/codecentric/boot/sample/Application.java
@@ -2,12 +2,16 @@ package com.codecentric.boot.sample;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
-@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableScheduling
 @EnableAdminServer
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
